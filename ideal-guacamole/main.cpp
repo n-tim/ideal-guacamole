d5a2085f5736e801468de13f9e410d46f96d260d
Timur Nasybullin <human12177@gmail.com>
2017-10-31 01:05:29 +0300

    Добавлен вывод в консоль

--- ideal-guacamole/main.cpp
+++ ideal-guacamole/main.cpp
@@ -1,10 +1,14 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include <QDebug>
+
 int main(int argc, char *argv[])
 {
   QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QGuiApplication app(argc, argv);
+
+  qDebug() << "hello!";
 
   QQmlApplicationEngine engine;
   engine.load(QUrl(QLatin1String("qrc:/main.qml")));
